eb6389a492bd74a8e4243921be5736dde90f2f3e
Narendra1128 <yadavnarendra1128@gmail.com>
2025-03-23 21:21:28 +0530

    pop

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "", // Replace 5000 with your backend port
-    },
-  },
   build: {
     outDir: "./dist",
   },
